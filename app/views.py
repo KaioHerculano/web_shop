f73cc898dc66f902d62cb947d6e24929887af46a
Kaio <kaioherculano12@gmail.com>
2025-05-27 17:50:11 -0400

    cards for fruits, groceries, salads, frozen foods and all were created and the banner was changed

--- app/views.py
+++ app/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from products.models import Product
+from categories.models import Category
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
+
         q = self.request.GET.get('q')
-        if q:
+        category_name = self.request.GET.get('category')
+
+        categories = Category.objects.all()
+        context['categories'] = categories
+
+        if category_name:
+            products = Product.objects.filter(category__name__iexact=category_name)
+        elif q:
             products = Product.objects.filter(title__icontains=q)
         else:
             products = Product.objects.all()
-        
+
         context['products'] = products
         context['query'] = q or ''
+        context['selected_category'] = category_name or ''
         return context
